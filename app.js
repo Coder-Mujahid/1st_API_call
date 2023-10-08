a756a00529ed1126506c68e4f7a9f12861e1be4a
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-09 02:39:25 +0600

    some change

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ function displaydata(data){
         div.innerHTML=`
         <h2 class="text-2xl text-center font-semibold mb-2">${id}</h2>
         <h2 class="text-xl text-center font-sm capitalize px-3 mb-2 text-red-500">${name}</h2>
-        <h2 class="text-xl flex items-center justify-center gap-2 mb-2"><i class='bx bxs-envelope text-red-700'></i>${email}</h2>
         <button onclick="showDetails(${id})" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-full">Details</button>
         
         `
@@ -51,6 +50,7 @@ function displaydata(data){
 }
 
 const div =document.createElement('div')
+
 function showDetails(id){
     const Details=document.getElementById('Details')
     fetch(`https://jsonplaceholder.typicode.com/comments/${id}`)
@@ -72,7 +72,6 @@ function showDetails(id){
         <h2 class="text-sm flex items-center justify-center gap-2 mb-2">about: ${title}</h2>
         `
         Details.appendChild(div)
-        console.log(Details);
     })
 }
 
